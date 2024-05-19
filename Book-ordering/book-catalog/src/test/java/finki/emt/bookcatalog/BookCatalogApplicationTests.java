8d30e8a78bed33f504fcda82fec1173e3bdb0d14
SamoilJakjimovski <153727426+SamoilJakjimovski@users.noreply.github.com>
2024-05-19 15:01:39 +0200

    Initial setup

---
+++ Book-ordering/book-catalog/src/test/java/finki/emt/bookcatalog/BookCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.emt.bookcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
